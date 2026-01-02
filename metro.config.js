50e084387cbbd57ce3ef7910789394208570995a
Youssef Mohamed <alamwele129@gmail.com>
2026-01-02 23:56:45 +0000

    Update code

--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,22 @@
+// metro.config.js
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// إضافة Aliases لإجبار التطبيق على استخدام shim.js
 config.resolver.extraNodeModules = {
   ...config.resolver.extraNodeModules,
-  // الضربة القاضية: استبدال مكتبة ws بملفنا البسيط
-  ws: require.resolve('./ws-shim.js'),
-  
-  // الاحتفاظ بالباقي كملفات فارغة
-  stream: require.resolve('./shim.js'),
-  http: require.resolve('./shim.js'),
-  https: require.resolve('./shim.js'),
-  net: require.resolve('./shim.js'),
-  tls: require.resolve('./shim.js'),
-  crypto: require.resolve('./shim.js'),
-  fs: require.resolve('./shim.js'),
+  'ws': require.resolve('./ws-shim.js'),
+  'url': require.resolve('./shim.js'),
+  'stream': require.resolve('./shim.js'),
+  'http': require.resolve('./shim.js'),
+  'https': require.resolve('./shim.js'),
+  'net': require.resolve('./shim.js'),
+  'tls': require.resolve('./shim.js'),
+  'crypto': require.resolve('./shim.js'),
+  'fs': require.resolve('./shim.js'),
+  'events': require.resolve('./shim.js'),
+  'zlib': require.resolve('./shim.js'),
 };
 
 module.exports = config;
